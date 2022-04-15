f89eb62946a2cebb182957a9b3769449df6d2df8
Maevenasss <maecenasss@gmail.com>
2022-04-15 18:37:15 +0300

    first commit

---
+++ JsScript/jsscript.js
@@ -0,0 +1,40 @@
+'use strict';
+/* const result = confirm ('Are you herе?'); */
+
+/* const answer = prompt('Вам есть 18?','18');
+console.log(typeof(answer)); */
+
+const answers = [];
+
+/* answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', ''); */
+
+/* document.write(answers); */
+console.log (typeof(answers));
+
+/* const user = 'Ivan';
+console.log (`Helo, ${user}`); */
+
+/*  Оператори: */
+/* префіксні та постфіксні */
+let incr = 10,
+    decr = 10;
+
+/* incr++;
+decr--; */
+
+console.log (++incr, --decr);
+
+/* Залишкове ділення: */
+console.log (5%2);
+
+/* && */
+/* const isChecked = true,
+      isClosed = true;
+console.log (isChecked && isClosed);  */
+
+/* || */
+const isChecked = true,
+      isClosed = false;
+console.log (isChecked || isClosed);  
